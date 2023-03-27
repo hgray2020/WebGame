0c2ae0d7a0b78ff5dba49ddd6d3b505a71bc8246
larrison23 <102928637+larrison23@users.noreply.github.com>
2023-03-27 18:31:33 -0400

    main menu

---
+++ TheWeb/Assets/Scripts/UI_Controller.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UI_Controller : MonoBehaviour
+{
+    public Button start_button;
+    public Button credit_button;
+    public Button exit_button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        start_button = root.Q<Button>("start_button");
+        credit_button = root.Q<Button>("credit_button");
+        exit_button = root.Q<Button>("exit_button");
+
+        start_button.clicked += StartButtonPressed;
+        credit_button.clicked += CreditButtonPressed;
+        exit_button.clicked += ExitButtonPressed;
+    }
+
+    // Update is called once per frame
+    void StartButtonPressed()
+    {
+        SceneManager.LoadScene("MaxScene");
+    }
+
+    void CreditButtonPressed() {
+        SceneManager.LoadScene("");
+    }
+
+    void ExitButtonPressed() {
+
+    }
+}
